b9e576decc07fd0098c78d2114aaf58b43330c36
darshan harihar <darshan.harihar@skypointcloud.com>
2025-01-15 00:26:56 +0530

    Added RSS feeds scraping

--- src/ingestion/scrapers/scraper_main.py
+++ src/ingestion/scrapers/scraper_main.py
@@ -1,22 +1,20 @@
-# scrapers/scraper_main.py
+from .site_scraper_config import RSS_CONFIGS
+from .rss_scraper import fetch_rss_feed
+from ..utils.logger import AppLogger
 
-from scrapers.site_scraper_config import SITE_CONFIGS
-from scrapers.site_scraper import scrape_website
-from utils.logger import get_logger
-
-logger = get_logger(__name__)
+logger = AppLogger(__name__)
 
 def run_all_scrapers():
     all_articles = []
 
-    for site_config in SITE_CONFIGS:
-        logger.info(f"Scraping {site_config['name']} ...")
-        site_articles = scrape_website(site_config)
-        logger.info(f"Found {len(site_articles)} articles from {site_config['name']}")
+    for rss_config in RSS_CONFIGS:
+        logger.info(f"Scraping {rss_config['name']}")
+        site_articles = fetch_rss_feed(rss_config['url'])
+        logger.info(f"Found {len(site_articles)} articles from {rss_config['name']}")
         
         # TODO: store site_articles in MinIO, DB, or local files
         # For example:
-        # store_in_minio(site_config["name"], site_articles)
+        # store_in_minio(rss_config["name"], site_articles)
         # update_metadata_db(site_articles)
 
         all_articles.extend(site_articles)
